e37cfd43eb0e2f11a8a338325d9280531dda0f6a
B-Nikola <nikola.bouattit@gmail.com>
2026-03-03 09:51:29 +0100

    Ajout du controle de la Led

--- tanks.ino
+++ tanks.ino
@@ -1,7 +1,7 @@
 #include <Adafruit_NeoPixel.h>
-#include <Arduino_JSON.h> // <--- La bibliothèque officielle Arduino_JSON
+#include <Arduino_JSON.h>
 
-// --- TANK 1 PINS ---
+// --- CONFIGURATION PINS ---
 #define T1_TRIG_G 10
 #define T1_ECHO_G 9
 #define T1_TRIG_D 6
@@ -13,7 +13,6 @@
 #define T1_BTN_FIR 11
 #define T1_LED_PIN 8
 
-// --- TANK 2 PINS (Tes nouvelles pins) ---
 #define T2_TRIG_G 34
 #define T2_ECHO_G 35
 #define T2_TRIG_D 40
@@ -25,82 +24,117 @@
 #define T2_BTN_FIR 23
 #define T2_LED_PIN 51
 
+#define LED_COUNT 8
+#define PRESS_THRESHOLD 200 
+
+Adafruit_NeoPixel strip1(LED_COUNT, T1_LED_PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel strip2(LED_COUNT, T2_LED_PIN, NEO_GRB + NEO_KHZ800);
+
 volatile int counter1 = 0;
 volatile int counter2 = 0;
-volatile int lastCLK1;
-volatile int lastCLK2;
 
 void setup() {
   Serial.begin(9600);
   
-  // Configuration Tank 1
+  strip1.begin(); strip1.show();
+  strip2.begin(); strip2.show();
+
+  // Tank 1 Setup
   pinMode(T1_TRIG_G, OUTPUT); pinMode(T1_ECHO_G, INPUT);
   pinMode(T1_TRIG_D, OUTPUT); pinMode(T1_ECHO_D, INPUT);
   pinMode(T1_SW_DIR, INPUT_PULLUP);
   pinMode(T1_ENC_CLK, INPUT); pinMode(T1_ENC_DT, INPUT);
   pinMode(T1_BTN_FIR, INPUT_PULLUP);
 
-  // Configuration Tank 2
+  // Tank 2 Setup
   pinMode(T2_TRIG_G, OUTPUT); pinMode(T2_ECHO_G, INPUT);
   pinMode(T2_TRIG_D, OUTPUT); pinMode(T2_ECHO_D, INPUT);
   pinMode(T2_SW_DIR, INPUT_PULLUP);
   pinMode(T2_ENC_CLK, INPUT); pinMode(T2_ENC_DT, INPUT);
   pinMode(T2_BTN_FIR, INPUT_PULLUP);
 
-  // Interruptions
-  lastCLK1 = digitalRead(T1_ENC_CLK);
-  lastCLK2 = digitalRead(T2_ENC_CLK);
+  // Interruptions prioritaires
   attachInterrupt(digitalPinToInterrupt(T1_ENC_CLK), updateEnc1, CHANGE);
   attachInterrupt(digitalPinToInterrupt(T2_ENC_CLK), updateEnc2, CHANGE);
+}
+
+// --- LOGIQUE LED SEGMENTÉE ---
+void updateLEDs(Adafruit_NeoPixel &strip, int pressure, int direction) {
+  // 1. ZONE CANON (LEDs 0 à 3) : Gère la pression
+  uint32_t canonColor;
+  if (pressure >= PRESS_THRESHOLD) {
+    canonColor = strip.Color(0, 255, 0); // VERT : Prêt à tirer
+  } else {
+    canonColor = strip.Color(255, 0, 0); // ROUGE : Pas assez de munitions
+  }
+  
+  for(int i = 0; i < 4; i++) {
+    strip.setPixelColor(i, canonColor);
+  }
+
+  // 2. ZONE PILOTAGE (LEDs 4 à 7) : Gère la direction
+  uint32_t driveColor;
+  if (direction == 1) {
+    driveColor = strip.Color(0, 0, 255);   // BLEU : Marche Avant
+  } else {
+    driveColor = strip.Color(255, 255, 0); // JAUNE : Marche Arrière
+  }
+
+  for(int i = 4; i < 8; i++) {
+    strip.setPixelColor(i, driveColor);
+  }
+
+  strip.show();
 }
 
 float getDist(int trig, int echo) {
   digitalWrite(trig, LOW); delayMicroseconds(2);
   digitalWrite(trig, HIGH); delayMicroseconds(10);
   digitalWrite(trig, LOW);
-  long duration = pulseIn(echo, HIGH, 25000); // Timeout court pour fluidité
-  if (duration == 0) return 999;
+  long duration = pulseIn(echo, HIGH, 25000);
+  if (duration == 0) return 10; // Max distance mappée
   int dist = duration * 0.034 / 2;
   return map(dist, 0, 30, 0, 10);
 }
 
 void loop() {
-  // Création de l'objet JSON racine
   JSONVar root;
-  
-  // Création du tableau "tanks"
   JSONVar tanks;
 
-  // --- DONNÉES TANK 1 ---
+  // --- TANK 1 ---
+  int p1 = map(analogRead(T1_PRESS), 1023, 0, 5, 1020);
+  int d1 = (int)!digitalRead(T1_SW_DIR);
+  
   JSONVar tank1;
   tank1["distG"] = getDist(T1_TRIG_G, T1_ECHO_G);
   tank1["distD"] = getDist(T1_TRIG_D, T1_ECHO_D);
-  tank1["pressure"] = (map(analogRead(T1_PRESS),1023,0,5,1020));
-  tank1["directionSwitch"] = (int)!digitalRead(T1_SW_DIR);
+  tank1["pressure"] = p1;
+  tank1["directionSwitch"] = d1;
   tank1["angleEncoder"] = counter1;
   tank1["shootButton"] = (int)!digitalRead(T1_BTN_FIR);
+  tanks[0] = tank1;
   
-  tanks[0] = tank1; // Ajout au tableau
+  updateLEDs(strip1, p1, d1);
 
-  delay(10); // Pause entre les deux lectures ultrasons
+  delay(10);
 
-  // --- DONNÉES TANK 2 ---
+  // --- TANK 2 ---
+  int p2 = map(analogRead(T2_PRESS), 1023, 0, 5, 1020);
+  int d2 = (int)!digitalRead(T2_SW_DIR);
+
   JSONVar tank2;
   tank2["distG"] = getDist(T2_TRIG_G, T2_ECHO_G);
   tank2["distD"] = getDist(T2_TRIG_D, T2_ECHO_D);
-  tank2["pressure"] = (map(analogRead(T2_PRESS),1023,0,5,1020));
-  tank2["directionSwitch"] = (int)!digitalRead(T2_SW_DIR);
+  tank2["pressure"] = p2;
+  tank2["directionSwitch"] = d2;
   tank2["angleEncoder"] = counter2;
   tank2["shootButton"] = (int)!digitalRead(T2_BTN_FIR);
-  
-  tanks[1] = tank2; // Ajout au tableau
+  tanks[1] = tank2;
 
-  // Assigner le tableau à l'objet racine
+  updateLEDs(strip2, p2, d2);
+
   root["tanks"] = tanks;
-
-  // Sérialisation et envoi
-  String jsonString = JSON.stringify(root);
-  Serial.println(jsonString);
+  Serial.println(JSON.stringify(root));
 
   delay(30); 
 }
